8011ad6c441aee0d729f9cfd5d6d307e085c9927
KozaX <55134615+KozaX@users.noreply.github.com>
2020-11-10 20:54:24 +0100

    refactor table header cells to make it more scalable

--- src/components/PlanetsList/index.tsx
+++ src/components/PlanetsList/index.tsx
@@ -1,52 +1,97 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react';
 
 //Styles
-import './PlanetsListStyle.scss'
+import './PlanetsListStyle.scss';
 
 //Components
-import PlanetsHeaderCell from './PlanetsHeaderCell'
+import PlanetsHeaderCell from './PlanetsHeaderCell';
 
 //Models
-import {PlanetsListProps} from './../../models/PlanetsList'
+import { PlanetsListProps } from './../../models/PlanetsList';
 
-const PlanetsList: React.FC<PlanetsListProps> = props => {
+const PlanetsList: React.FC<PlanetsListProps> = ({ planetsList }) => {
+  const uknownStr = 'unknown';
+  const [list, setList] = useState(planetsList);
+  const [isSorting, setIsSorting] = useState<string>('');
 
-  const uknownStr = 'unknown'
-  const [list, setList] = useState(props.planetsList);
-  const [isSorting, setIsSorting] = useState<string>('')
-
+  const arrayOfTableHeaders = [
+    'Planet Name',
+    'Rotation Period',
+    'Orbital Period',
+    'Diameter',
+    'Climates',
+    'Surface Water',
+    'Population'
+  ];
 
   return (
     <table>
       <thead>
         <tr>
-          {/*Passing the state and the setters in order to manage them in the component reponsible for changing them*/}
-          <PlanetsHeaderCell headerTitle="Planet Name" isActive={isSorting === 'name'}  planetsList={list} listSetter={setList} isSorting={isSorting} setIsSorting={setIsSorting} category={"name"}/>
-          <PlanetsHeaderCell headerTitle="Rotation Period" isActive={isSorting === 'rotationPeriod'}  planetsList={list} listSetter={setList} isSorting={isSorting} setIsSorting={setIsSorting} category={"rotationPeriod"}/>
-          <PlanetsHeaderCell headerTitle="Orbital Period" isActive={isSorting === 'orbitalPeriod'}  planetsList={list} listSetter={setList} isSorting={isSorting} setIsSorting={setIsSorting} category={"orbitalPeriod"}/>
-          <PlanetsHeaderCell headerTitle="Diameter" isActive={isSorting === 'diameter'}  planetsList={list} listSetter={setList} isSorting={isSorting} setIsSorting={setIsSorting} category={"diameter"}/>
-          <PlanetsHeaderCell headerTitle="Climate" isActive={isSorting === 'climates'}  planetsList={list} listSetter={setList} isSorting={isSorting} setIsSorting={setIsSorting} category={"climates"}/>
-          <PlanetsHeaderCell headerTitle="Surface Water" isActive={isSorting === 'surfaceWater'}  planetsList={list} listSetter={setList} isSorting={isSorting} setIsSorting={setIsSorting} category={"surfaceWater"}/>
-          <PlanetsHeaderCell headerTitle="Population" isActive={isSorting === 'population'}  planetsList={list} listSetter={setList} isSorting={isSorting} setIsSorting={setIsSorting} category={"population"}/>
+          {arrayOfTableHeaders.map((item, index) => {
+            let category = '';
+            if (!index) category = 'name';
+            if (item.includes(' ')) {
+              const arrayOfStrings = item.split(' ');
+              category = arrayOfStrings
+                .map((value, index) => (!index ? value.toLowerCase() : value))
+                .join('');
+            } else {
+              category = item.toLowerCase();
+            }
+            return (
+              <PlanetsHeaderCell
+                headerTitle={item}
+                isActive={isSorting === category}
+                planetsList={list}
+                listSetter={setList}
+                isSorting={isSorting}
+                setIsSorting={setIsSorting}
+                category={category}
+              />
+            );
+          })}
         </tr>
       </thead>
       <tbody>
         {list.map(value => {
           return (
             <tr key={value.id}>
-              <td className={(isSorting === 'name' && 'isSorting') || ''}>{value.name}</td>
-              <td className={(isSorting === 'rotationPeriod' && 'isSorting') || ''}>{value.rotationPeriod || uknownStr}</td>
-              <td className={(isSorting === 'orbitalPeriod' && 'isSorting') || ''}>{value.orbitalPeriod || uknownStr}</td>
-              <td className={(isSorting === 'diameter' && 'isSorting') || ''}>{value.diameter || uknownStr}</td>
-              <td className={(isSorting === 'climates' && 'isSorting') || ''}>{value.climates.map(value => <p key={Math.random() /*Not best practice, but will do in this small examaple*/ }>{value}</p>)}</td>
-              <td className={(isSorting === 'surfaceWater' && 'isSorting') || ''}>{value.surfaceWater || uknownStr}</td>
-              <td className={(isSorting === 'population' && 'isSorting') || ''}>{value.population || uknownStr}</td>
+              <td className={isSorting === 'name' ? 'isSorting' : ''}>
+                {value.name}
+              </td>
+              <td className={isSorting === 'rotationPeriod' ? 'isSorting' : ''}>
+                {value.rotationPeriod || uknownStr}
+              </td>
+              <td className={isSorting === 'orbitalPeriod' ? 'isSorting' : ''}>
+                {value.orbitalPeriod || uknownStr}
+              </td>
+              <td className={isSorting === 'diameter' ? 'isSorting' : ''}>
+                {value.diameter || uknownStr}
+              </td>
+              <td className={isSorting === 'climates' ? 'isSorting' : ''}>
+                {value.climates.map((value, index) => (
+                  <p
+                    key={
+                      index /*Not best practice, but will do in this small examaple*/
+                    }
+                  >
+                    {value}
+                  </p>
+                ))}
+              </td>
+              <td className={isSorting === 'surfaceWater' ? 'isSorting' : ''}>
+                {value.surfaceWater || uknownStr}
+              </td>
+              <td className={isSorting === 'population' ? 'isSorting' : ''}>
+                {value.population || uknownStr}
+              </td>
             </tr>
-          )
+          );
         })}
       </tbody>
     </table>
-  )
-}
+  );
+};
 
 export default PlanetsList;
