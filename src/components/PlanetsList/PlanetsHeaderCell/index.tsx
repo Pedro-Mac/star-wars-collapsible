606069cab1d27b7a22e0ce7c03f5c1fe7bc8ecf4
KozaX <55134615+KozaX@users.noreply.github.com>
2020-11-10 22:41:12 +0100

    refactor key types on planets header cell component to make it stricter

--- src/components/PlanetsList/PlanetsHeaderCell/index.tsx
+++ src/components/PlanetsList/PlanetsHeaderCell/index.tsx
@@ -1,72 +1,96 @@
-import React, {useState} from 'react'
-
+import React, { useState } from 'react';
 
 //Models
-import {DispatcherProps} from './../../../models/Dispatchers';
-import { PlanetsListProps } from './../../../models/PlanetsList'
-interface HeaderProps extends DispatcherProps, PlanetsListProps{
+import { DispatcherProps } from './../../../models/Dispatchers';
+import { PlanetsListProps } from './../../../models/PlanetsList';
+import { SinglePlanetInfo } from './../models/SinglePlanetInfo';
+interface HeaderProps extends DispatcherProps, PlanetsListProps {
   headerTitle: string;
   isSorting: string;
   category: string;
   isActive: boolean;
-  
 }
 
 const PlanetsHeaderCell: React.FC<HeaderProps> = props => {
-  
   const [sortingDirection, setSortingDirection] = useState<string>('');
 
   const handleSorting = (direction: 'ascending' | 'descending') => {
-
-    setSortingDirection(()=> direction)
+    setSortingDirection(() => direction);
     console.log(sortingDirection);
-  }
+  };
 
-
-  const handleTableSorting = (key: string, direction: 'ascending' | 'descending' ) => {
-    switch(direction) {
+  const handleTableSorting = (
+    key: string,
+    direction: 'ascending' | 'descending'
+  ) => {
+    switch (direction) {
       case 'descending':
-        const descendingList = props.planetsList.sort((a, b)=> {
-          if((b as any)[key] > (a as any)[key]){
-            return -1;
-          } else if ((b as any)[key] < (a as any)[key]) {
-            return 1;
-          } else {
-            return 0;
-          }
-        })
-        handleSorting('descending')
-        props.listSetter(() => [...descendingList]);
-        break;
-        case 'ascending':
-          const ascendingList = props.planetsList.sort((a, b)=> {
-            if((b as any)[key] < (a as any)[key]){
+        const descendingList = props.planetsList.sort(
+          (a: SinglePlanetInfo, b: SinglePlanetInfo) => {
+            if ((b as any)[key] > (a as any)[key]) {
               return -1;
-            } else if ((b as any)[key] > (a as any)[key]) {
+            } else if (b[key]! < a[key]!) {
               return 1;
             } else {
               return 0;
             }
-          })
-          handleSorting('ascending')
-          props.listSetter(() => [...ascendingList]);
-          break;
+          }
+        );
+        handleSorting('descending');
+        props.listSetter(() => [...descendingList]);
+        break;
+      case 'ascending':
+        const ascendingList = props.planetsList.sort((a, b) => {
+          if ((b as any)[key] < (a as any)[key]) {
+            return -1;
+          } else if ((b as any)[key] > (a as any)[key]) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+        handleSorting('ascending');
+        props.listSetter(() => [...ascendingList]);
+        break;
     }
-    props.setIsSorting(()=> key);
-  }
-
+    props.setIsSorting(() => key);
+  };
 
   return (
-      <th>
-        <div>
-          <p className={(props.isActive && 'isActive') || ''}>{props.headerTitle}</p>
-          <div className="arrows-container">
-            <span style={{color: (props.isActive && sortingDirection==='ascending' && '#00687f') || ''}} onClick={()=> handleTableSorting(props.category,'ascending')}>▲</span>
-            <span style={{color: (props.isActive && sortingDirection==='descending' && '#00687f') || ''}} onClick={()=> handleTableSorting(props.category,'descending')}>▼</span>
-          </div>
+    <th>
+      <div>
+        <p className={(props.isActive && 'isActive') || ''}>
+          {props.headerTitle}
+        </p>
+        <div className="arrows-container">
+          <span
+            style={{
+              color:
+                (props.isActive &&
+                  sortingDirection === 'ascending' &&
+                  '#00687f') ||
+                ''
+            }}
+            onClick={() => handleTableSorting(props.category, 'ascending')}
+          >
+            ▲
+          </span>
+          <span
+            style={{
+              color:
+                (props.isActive &&
+                  sortingDirection === 'descending' &&
+                  '#00687f') ||
+                ''
+            }}
+            onClick={() => handleTableSorting(props.category, 'descending')}
+          >
+            ▼
+          </span>
         </div>
-      </th>
-  )
-}
+      </div>
+    </th>
+  );
+};
 
-export default PlanetsHeaderCell
+export default PlanetsHeaderCell;
