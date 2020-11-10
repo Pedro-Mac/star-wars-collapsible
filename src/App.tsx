184b6c3857ff5ac0a53ca871f9f7cc1e148f6c4b
KozaX <55134615+KozaX@users.noreply.github.com>
2020-11-10 22:44:52 +0100

    add github repository link

--- src/App.tsx
+++ src/App.tsx
@@ -3,38 +3,45 @@ import React from 'react';
 import './App.scss';
 
 //Components
-import MovieContainer  from './components/Movies/MovieContainer'
+import MovieContainer from './components/Movies/MovieContainer';
 
 //JSON Data
 import starWars from './data.json';
-
-
-
 
 const App: React.FC = () => {
   const films = starWars.data.films;
   const planets = starWars.data.planets;
 
-  const getPlanetsInMovie = (movie: {id: string, title: string}) => {
+  const getPlanetsInMovie = (movie: { id: string; title: string }) => {
     const planetsList = [];
 
     for (let element of planets) {
-      const filmsList = element.filmConnection.films
+      const filmsList = element.filmConnection.films;
       for (let film of filmsList) {
         if (film.id === movie.id) planetsList.push(element);
       }
     }
 
     return planetsList;
-  }
-
+  };
 
   return (
     <div className="App">
-     <img src="./assets/LOGO.svg" alt="LOGO"/>
-      {films.map(value => <MovieContainer key={value.id} movie={value} handlePlanetsList={() => getPlanetsInMovie(value)} />)}
+      <img src="./assets/LOGO.svg" alt="LOGO" />
+      {films.map(value => (
+        <MovieContainer
+          key={value.id}
+          movie={value}
+          handlePlanetsList={() => getPlanetsInMovie(value)}
+        />
+      ))}
+      <p>
+        <a href="https://github.com/Pedro-Mac/star-wars-collapsible">
+          Github repo
+        </a>
+      </p>
     </div>
   );
-}
+};
 
 export default App;
